af6b76bca9348cd07a5d885c62f2fa54ad23f34e
chengwei <chengwei@rlsk.link>
2020-09-08 17:20:12 +0800

    更新

---
+++ poto.py
@@ -0,0 +1,28 @@
+import asyncio
+import random
+
+
+class Potato:
+    @classmethod
+    def make(cls, num, *args, **kws):
+        potatos = []
+        for i in range(num):
+            potatos.append(cls.__new__(cls, *args, **kws))
+        return potatos
+
+all_potatos = Potato.make(5)
+
+async def take_potatos(num):
+    count = 0
+    while True:
+        if len(all_potatos) == 0:
+            await ask_for_potato()
+        potato = all_potatos.pop()
+        yield potato
+        count += 1
+        if count == num:
+            break
+
+async def ask_for_potato():
+    await asyncio.sleep(random.random())
+    all_potatos.extend(Potato.make(random.randint(1, 10)))
---
+++ www/orm.py
@@ -0,0 +1,35 @@
+import logging; logging.basicConfig(level=logging.INFO)
+import aiomysql
+
+def log(sql, args=()):
+    logging.info('SQL: %s' % sql)
+
+async def create_pool(loop,**kw):
+    logging.info('create database connection pool...')
+    global __pool
+    __pool = await aiomysql.create_pool(
+        host=kw.get('host', 'localhost'),
+        port=kw.get('port', 3306),
+        user=kw['user'],
+        password=kw['password'],
+        db=kw['db'],
+        charset=kw.get('charset', 'utf8'),
+        autocommit=kw.get('autocommit', True),
+        maxsize=kw.get('maxsize', 10),
+        minsize=kw.get('minsize', 1),
+        loop=loop
+    )
+
+async def select(sql,args,size=None):
+    log(sql,args)
+    global __pool
+    async with __pool.get() as comm:
+        async with comm.cursor() as cur:
+            await cur.execute(sql.replace('?', '%s'), args or ())
+            if size:
+                #返回指定条数的数据
+                rs = await cur.fetchmany(size)
+            else:
+                #返回全部数据
+                rs = await cur.fetchall()
+            return rs
